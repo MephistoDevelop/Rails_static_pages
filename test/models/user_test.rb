386fe623402ca2eba297cc1b088efa0a8278cf38
Cristian <inesherald@gmail.com>
2019-08-28 13:01:40 -0500

    add test model and model data

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -5,7 +5,7 @@ class UserTest < ActiveSupport::TestCase
   #   assert true
   # endde
   def setup
-    @user = User.new(name:"Ines",email:"Develops")
+    @user = User.new(name:"Ines",email:"user@example.com")
   end
 
   test "should be valid" do
@@ -30,6 +30,15 @@ class UserTest < ActiveSupport::TestCase
   test "email should not be too long" do
     @user.email = "a"*244 + "@example.com"
     assert_not @user.valid?
+  end
+
+  test "email validation should reject invalid addresses" do
+    invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |invalid_address|
+      @user.email = invalid_address
+      assert_not @user.valid?, "#{invalid_address.inspect} should be invalid"
+    end
   end
 
 end
