ec7482db9ee414769b3267b7ddb5f77ac91314bd
Cristian <inesherald@gmail.com>
2019-09-02 17:13:49 -0500

    fixed linterns errors

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,31 +10,31 @@ class UserTest < ActiveSupport::TestCase
 
   end
 
-  test "should be valid" do
+  test'should be valid' do
     assert @user.valid?
   end
 
-  test "name should be present " do
+  test 'name should be present ' do
     @user.name = "     "
     assert_not @user.valid?
   end
 
-  test "email should be present" do
+  test 'email should be present' do
     @user.email="     "
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
+  test 'name should not be too long' do
     @user.name="a"*51
     assert_not @user.valid?
   end
 
-  test "email should not be too long" do
+  test 'email should not be too long' do
     @user.email = "a"*244 + "@example.com"
     assert_not @user.valid?
   end
 
-  test "email validation should reject invalid addresses" do
+  test 'email validation should reject invalid addresses' do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
@@ -43,14 +43,14 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
-  test "email should be unique" do
+  test 'email should be unique' do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
   end
 
-  test "email address should b saved as a lower case" do
+  test 'email address should b saved as a lower case' do
 
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
@@ -58,12 +58,12 @@ class UserTest < ActiveSupport::TestCase
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
 
-  test "Password Should be Present (non blank)" do
+  test 'Password Should be Present (non blank)' do
     @user.password = @user.password_confirmation = " "*6
     assert_not @user.valid?
   end
 
-  test "password should have a minumum length" do
+  test 'password should have a minumum length' do
     @user.password = @user.password_confirmation = "a"*5
     assert_not @user.valid?
   end
