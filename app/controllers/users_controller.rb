a6e074ab25ef7a36131d56bc65e660861fbb45fa
Cristian <inesherald@gmail.com>
2019-09-11 15:41:50 -0500

    fix tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,22 +4,22 @@ class UsersController < ApplicationController
   def new
     @users = User.new
   end
+
   def show
     @users = User.find(params[:id])
-    #debugger
+    # debugger
   end
+
   def index
     @users = User.all
   end
+
   def create
     @users = User.new(params[:id])
     if @users.save
-      #handle a suscessfull action
+      # handle a suscessfull action
     else
       render 'new'
     end
   end
 end
-
-
-
