cfbf4cfb0b7fd4efed5bb4c6332956a948eb2ffa
Cristian <inesherald@gmail.com>
2019-09-12 18:36:23 -0500

    add current_user helper

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,6 +7,8 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user &&  user.authenticate(params[:session][:password])
       #log the user and redirect to users page
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -2,4 +2,11 @@ module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
+
+   # Returns the current logged-in user (if any).
+   def current_user
+    if session[:user_id]
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+  end
 end
