6191be450ffd679b3c7cd4f444042b6c5e70c63e
Cristian <inesherald@gmail.com>
2019-09-12 18:14:33 -0500

    add login helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  include Sessionshelper
   def hello
     render html: "hello,world!, i'm mephistoDevelops !!"
   end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     if user &&  user.authenticate(params[:session][:password])
       #log the user and redirect to users page
     else
-      flash[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
