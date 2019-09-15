20706350c5a2cd6f67c367c0881872182e30bc5e
Cristian <inesherald@gmail.com>
2019-09-15 06:35:02 -0500

    Finish advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,16 +12,15 @@ class SessionsController < ApplicationController
     if user&.authenticate(params[:session][:password])
       # log the user and redirect to users page
       log_in user
-      remember user
+      params[:session][:remember_me] == 1 ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
     end
   end
-
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,11 +5,6 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
-
   # Remembers a user in a persistent session.
   def remember(user)
     user.remember_token
@@ -22,6 +17,7 @@ module SessionsHelper
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: user_id)
     elsif (user_id = cookies.signed[:user_id])
+      raise       # The tests still pass, so this branch is currently untested.
       user = User.find_by(id: user_id)
       if user && user.authenticated?(cookies[:remember_token])
         log_in user
@@ -33,5 +29,19 @@ module SessionsHelper
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
     !current_user.nil?
+  end
+
+  # Forgets a persistent session.
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+
+
+  def log_out
+    forget(current_user)
+    session.delete(:user_id)
+    @current_user = nil
   end
 end
